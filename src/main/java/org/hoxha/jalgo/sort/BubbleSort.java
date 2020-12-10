7e1c09a9e7ac2e4cb3d410e8bd9cbc11c4c7d3f2
Hoxha <armend.hoxha@rabobank.nl>
2020-12-10 21:10:34 +0100

    Implements two methods in the BubbleSort class: - sorting arrays of Comparable objects - sorting lists of Comparable objects

--- src/main/java/org/hoxha/jalgo/sort/BubbleSort.java
+++ src/main/java/org/hoxha/jalgo/sort/BubbleSort.java
@@ -3,33 +3,90 @@ package org.hoxha.jalgo.sort;
 import static org.hoxha.jalgo.sort.SortingOrder.ASCENDING;
 import static org.hoxha.jalgo.sort.SortingOrder.DESCENDING;
 
-final class BubbleSort implements Sort{
-	@Override
-	public void sortAscending(int[] array) {
-		sort(array, ASCENDING);
-	}
+import java.util.List;
 
-	@Override
-	public void sortDescending(int[] array) {
-		sort(array, DESCENDING);
-	}
+final class BubbleSort implements Sort {
+    @Override
+    public void sortAscending(int[] array) {
+        sort(array, ASCENDING);
+    }
 
-	private void sort(int[] a, SortingOrder order) {
-		boolean swapped = false;
-		for (int i = 0; i < a.length; i++) {
-			swapped = false;
-			for (int j = a.length - 1; j > i; j--) {
-				if ((order == ASCENDING && a[j] < a[j - 1])
-						|| (order == DESCENDING && a[j] > a[j - 1])) {
-					int temp = a[j];
-					a[j] = a[j - 1];
-					a[j - 1] = temp;
-					swapped = true;
-				}
-			}
-			if (!swapped) {
-				break;
-			}
-		}
-	}
+    @Override
+    public void sortDescending(int[] array) {
+        sort(array, DESCENDING);
+    }
+
+    @Override
+    public <T extends Comparable<T>> void sortAscending(T[] array) {
+        sort(array, ASCENDING);
+    }
+
+    @Override
+    public <T extends Comparable<T>> void sortDescending(T[] array) {
+        sort(array, DESCENDING);
+    }
+
+    @Override
+    public <T extends Comparable<T>> void sortAscending(List<T> list) {
+        sort(list, ASCENDING);
+    }
+
+    @Override
+    public <T extends Comparable<T>> void sortDescending(List<T> list) {
+        sort(list, DESCENDING);
+    }
+
+    private void sort(int[] array, SortingOrder order) {
+        boolean swapped;
+        for (int i = 0; i < array.length; i++) {
+            swapped = false;
+            for (int j = array.length - 1; j > i; j--) {
+                if ((order == ASCENDING && array[j] < array[j - 1]) || (order == DESCENDING && array[j] > array[j - 1])) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    private <T extends Comparable<T>> void sort(T[] array, SortingOrder order) {
+        boolean swapped;
+        for (int i = 0; i < array.length; i++) {
+            swapped = false;
+            for (int j = array.length - 1; j > i; j--) {
+                if ((order == ASCENDING && array[j].compareTo(array[j - 1]) < 0) || (order == DESCENDING && array[j].compareTo(array[j - 1]) > 0)) {
+                    T temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    private <T extends Comparable<T>> void sort(List<T> list, SortingOrder order) {
+        boolean swapped;
+        for (int i = 0; i < list.size(); i++) {
+            swapped = false;
+            for (int j = list.size() - 1; j > i; j--) {
+                if ((order == ASCENDING && list.get(j).compareTo(list.get(j - 1)) < 0) || (order == DESCENDING && list.get(j).compareTo(list.get(j - 1)) > 0)) {
+                    T temp = list.get(j);
+                    list.set(j, list.get(j - 1));
+                    list.set(j - 1, temp);
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
 }
--- src/test/java/org/hoxha/jalgo/sort/BubbleSortTest.java
+++ src/test/java/org/hoxha/jalgo/sort/BubbleSortTest.java
@@ -1,27 +1,9 @@
 package org.hoxha.jalgo.sort;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+class BubbleSortTest extends AbstractSortTest {
 
-import org.junit.jupiter.api.Test;
-
-class BubbleSortTest {
-
-    private final Sort sut = SortFactory.bubbleSort();
-
-    @Test
-    void testArrayAscending() {
-        int[] a = { 3, 1, 8 };
-
-        sut.sortAscending(a);
-
-        assertThat(a).isEqualTo(new int[] { 1, 3, 8 });
-    }
-
-    @Test
-    void testArrayDescending() {
-        int[] a = { 3, 1, 8 };
-        sut.sortDescending(a);
-
-        assertThat(a).isEqualTo(new int[] { 8, 3, 1 });
+    @Override
+    public Sort getInstance() {
+        return SortFactory.bubbleSort();
     }
 }
